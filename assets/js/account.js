d9b836536a09438e349b300626e997973f0849fc
Trezalore <53103532+Trezalore@users.noreply.github.com>
2020-04-30 15:18:26 -0300

    arrumando a requisição
    processo de requisição concluido e ajustado, faltando apenas guarda as informações no storage

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,43 +1,53 @@
-const minhaPromise = function(url) {
+function minhaPromise(url) {
   return new Promise(function(resolve, reject) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
+    let xhr = new XMLHttpRequest(); 
+    xhr.open('GET', `../php/main.php?${url}`);
     xhr.send(null);
+
     xhr.onreadystatechange = function() {
       if(xhr.readyState === 4) {
         if(xhr.status === 200) {
           resolve(JSON.parse(xhr.responseText));
         } else {
-          reject(xhr.status);
+          reject(xhr);
         }
       }
     }
   });
 }
 
-let login = document.querySelector('.login input[type="submit"]');
-let cad = document.querySelector('.cadastrar input[type="submit"]');
+let login = document.querySelector('.login');
+let cad = document.querySelector('.cadastrar');
 
-let logEmail = document.querySelector('.login input[type="email"]');
-let logSenha = document.querySelector('.login input[type="password"]');
+let logEmail = document.getElementById('logEmail');
+let logSenha = document.getElementById('logSenha');
 
-login.onclick = function() {
-  let logUrl = `http://localhost/Unidez-Nove/assets/php/main.php?logEmail=${logEmail.value}&logSenha=${logSenha.value}`;
-  minhaPromise(logUrl)
+let cadNome = document.getElementById('cadNome');
+let cadEmail = document.getElementById('cadEmail');
+let cadSenha = document.getElementById('cadSenha');
+let cadConSenha = document.getElementById('cadConSenha');
+
+login.addEventListener('submit', function(event) {
+  event.preventDefault();
+  minhaPromise(`logEmail=${logEmail.value}&logSenha=${logSenha.value}`)
     .then(function(data) {
       console.log(data);
     })
     .catch(function(err) {
-      console.log(err)
+      console.warn(err);
     });
-}
+})
 
-let cadNome = document.querySelector('.cadastrar input[type="text"]');
-let cadEmail = document.querySelector('.cadastrar input[type="email"]');
-let cadSenha = document.querySelectorAll('.cadastrar input[type="password"]');
-cad.onclick = function() {
-  if(cadSenha[0].value == cadSenha[1].value) {
-    let cadUrl = `http://localhost/Unidez-Nove/assets/php/main.php?cadNome=${cadNome.value}&cadEmail=${cadEmail.value}&cadSenha=${cadSenha[0].value}`; 
-    console.log(cadUrl);
+cad.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if(cadSenha.value == cadConSenha.value) {
+    let cadUrl = `cadNome=${cadNome.value}&cadEmail=${cadEmail.value}&cadSenha=${cadSenha.value}`; 
+    minhaPromise(cadUrl)
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function(err) {
+      console.warn(err);
+    });
   }
-}
+})
